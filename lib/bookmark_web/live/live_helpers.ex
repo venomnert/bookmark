6609c34d2a197f54641930a9ca38bf6bcc9448f3
venomnert <neerthigan@gmail.com>
2020-12-31 10:07:37 -0500

    Created separate form component for contexts

--- lib/bookmark_web/live/live_helpers.ex
+++ lib/bookmark_web/live/live_helpers.ex
@@ -15,9 +15,14 @@ defmodule BookmarkWeb.LiveHelpers do
         bookmarks: @bookmarks,
         return_to: Routes.bookmarks_index_path(@socket, :index) %>
   """
-  def live_modal(socket, component, opts) do
+  def live_modal_bookmark(socket, component, opts) do
     path = Keyword.fetch!(opts, :return_to)
-    modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
+    modal_opts = [id: :modal_bookmark, return_to: path, component: component, opts: opts]
+    live_component(socket, BookmarkWeb.ModalComponent, modal_opts)
+  end
+  def live_modal_context(socket, component, opts) do
+    path = Keyword.fetch!(opts, :return_to)
+    modal_opts = [id: :modal_context, return_to: path, component: component, opts: opts]
     live_component(socket, BookmarkWeb.ModalComponent, modal_opts)
   end
 end
