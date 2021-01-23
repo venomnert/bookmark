2c5fd5a5000d46dfa2f4dd4400b98f6b7e77bac9
venomnert <neerthigan@gmail.com>
2021-01-22 19:07:46 -0500

    Removed event and added to form

--- lib/bookmark_web/live/bookmarks_live/edit.ex
+++ lib/bookmark_web/live/bookmarks_live/edit.ex
@@ -18,13 +18,4 @@ defmodule BookmarkWeb.BookmarksLive.Edit do
     |> assign(:page_title, "Edit Bookmarks")
     |> assign(:bookmarks, Core.get_bookmarks_with_context!(id))
   end
-
-  @impl true
-  def handle_event("delete-context", %{"id" => context_id}, socket) do
-
-    updated_bookmark =
-      socket.assigns.bookmarks |> Core.delete_context_from_bookmark(String.to_integer(context_id))
-
-    {:noreply, assign(socket, :bookmarks, updated_bookmark)}
-  end
 end
